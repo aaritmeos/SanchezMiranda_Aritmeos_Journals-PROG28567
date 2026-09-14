4dc4b1eec5a67be0df8c328fa86cbccb7e5a6aa0
aaritmeos <sancheez@sheridancollege.ca>
2026-09-13 22:22:43 -0400

    Saved last changes

--- Assets/Scripts/Pipeline.cs
+++ Assets/Scripts/Pipeline.cs
@@ -7,22 +7,17 @@ public class Pipeline : MonoBehaviour
 {
     List<Vector2> lastMousePos = new List<Vector2>();
     float timer = 0f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    {       }
     void Update()
     {
        if (Mouse.current.leftButton.isPressed == true)
         {
             lastMousePos.Add((Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue())));
-            timer += 1f * Time.deltaTime;
+            timer += 0.1f * Time.deltaTime;
             int last = lastMousePos.Count;
 
-            if (timer == 1f && Mouse.current.leftButton.isPressed == true)
+            if (timer == 0.1f && Mouse.current.leftButton.isPressed == true)
             {
                 timer = 0f;
                 Debug.DrawLine(lastMousePos[last], (Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue())), Color.red);
