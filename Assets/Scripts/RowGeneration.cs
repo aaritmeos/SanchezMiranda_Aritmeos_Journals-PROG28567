a376cda18dc24c9d19b969109b9daf101230a41f
aaritmeos <sancheez@sheridancollege.ca>
2026-09-13 21:23:56 -0400

    Task 2.3
    Added a check to verify if the input from SquareNumberInput can be used as an integer and limit for the for statement that draws the squares before drawing the squares

--- Assets/Scripts/RowGeneration.cs
+++ Assets/Scripts/RowGeneration.cs
@@ -1,49 +1,52 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class RowGeneration : MonoBehaviour
 {
+    //integers to be able to move the sqaures
     public int startPointA = 1;
     public int startPointB = -1;
+    //variable to grab the text from the input field
     public TMP_InputField squareNumber;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    {}
     void Update()
-    {
-        
-        
-    }
-
+    {}
     //function to create a row of squares upon hitting the generate button
     public void Generate ()
     {
+        //convert input to string
         string squNum = squareNumber.text;
-        int num = int.Parse(squNum);
-        //loop to start and stop the generation of squares
-        for (int i = 0; i < num; i++)
+        //variable to convert string to integer
+        int num;
+        //check if string can be Parsed
+        bool result = int.TryParse(squNum, out num);
+        //if it can't be Parsed, send invalid message to the console
+        if (result == false)
         {
-            
-            //vectors to create the squares
-            Vector2 firstQuad = new Vector2(startPointA, 1);
-            Vector2 secondQuad = new Vector2(startPointA, -1);
-            Vector2 thirdQuad = new Vector2(startPointB, -1);
-            Vector2 fourthQuad = new Vector2(startPointB, 1);
-
-
-            //draw the lines that create the squares
-            Debug.DrawLine(firstQuad, secondQuad, Color.white, 10f);
-            Debug.DrawLine(secondQuad, thirdQuad, Color.white, 10f);
-            Debug.DrawLine(thirdQuad, fourthQuad, Color.white, 10f);
-            Debug.DrawLine(fourthQuad, firstQuad, Color.white, 10f);
-
-            startPointA += 2;
-            startPointB += 2;
+            Debug.Log("invalid");
+        }
+        //if it can be Parsed, use the new integer as limit for the for loop
+        if (result == true)
+        {
+            //loop to start and stop the generation of squares, uses the int from above as limit
+            for (int i = 0; i < num; i++)
+            {
+                //vectors to create the squares
+                Vector2 firstQuad = new Vector2(startPointA, 1);
+                Vector2 secondQuad = new Vector2(startPointA, -1);
+                Vector2 thirdQuad = new Vector2(startPointB, -1);
+                Vector2 fourthQuad = new Vector2(startPointB, 1);
+                //draw the lines that create the squares
+                Debug.DrawLine(firstQuad, secondQuad, Color.white, 10f);
+                Debug.DrawLine(secondQuad, thirdQuad, Color.white, 10f);
+                Debug.DrawLine(thirdQuad, fourthQuad, Color.white, 10f);
+                Debug.DrawLine(fourthQuad, firstQuad, Color.white, 10f);
+                startPointA += 2;
+                startPointB += 2;
+            }
         }
     }
 }
