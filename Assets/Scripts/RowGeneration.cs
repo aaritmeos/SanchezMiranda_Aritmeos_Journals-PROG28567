32d486599b3ac262fd3cb3fae173e835ef630c27
aaritmeos <sancheez@sheridancollege.ca>
2026-09-13 20:42:52 -0400

    Task 2.1 and 2.2
    During the first chunk of time, I implemented the appearance of side-by-side squares using Debug.DrawLine, a for statement with an arbitrary limit, and starting points of the squares that moved to the left with each cycle.
    
    During the second chunk of time, I implemented the modification of the number squares by grabbing the Input from the SquareNumberInput in the UI, and using Parse to turn it into an integer used as the limit of the for statement.

--- Assets/Scripts/RowGeneration.cs
+++ Assets/Scripts/RowGeneration.cs
@@ -1,7 +1,12 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RowGeneration : MonoBehaviour
 {
+    public int startPointA = 1;
+    public int startPointB = -1;
+    public TMP_InputField squareNumber;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +17,33 @@ public class RowGeneration : MonoBehaviour
     void Update()
     {
         
+        
+    }
+
+    //function to create a row of squares upon hitting the generate button
+    public void Generate ()
+    {
+        string squNum = squareNumber.text;
+        int num = int.Parse(squNum);
+        //loop to start and stop the generation of squares
+        for (int i = 0; i < num; i++)
+        {
+            
+            //vectors to create the squares
+            Vector2 firstQuad = new Vector2(startPointA, 1);
+            Vector2 secondQuad = new Vector2(startPointA, -1);
+            Vector2 thirdQuad = new Vector2(startPointB, -1);
+            Vector2 fourthQuad = new Vector2(startPointB, 1);
+
+
+            //draw the lines that create the squares
+            Debug.DrawLine(firstQuad, secondQuad, Color.white, 10f);
+            Debug.DrawLine(secondQuad, thirdQuad, Color.white, 10f);
+            Debug.DrawLine(thirdQuad, fourthQuad, Color.white, 10f);
+            Debug.DrawLine(fourthQuad, firstQuad, Color.white, 10f);
+
+            startPointA += 2;
+            startPointB += 2;
+        }
     }
 }
--- Assets/Scripts/SquareSpawner.cs
+++ Assets/Scripts/SquareSpawner.cs
@@ -5,20 +5,14 @@ public class SquareSpawner : MonoBehaviour
 {
     public int posOne = 1;
     public int negOne = -1;
-   
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
         //Vector that stores the state of the scrolling wheel
         Vector2 changeSize = Mouse.current.scroll.ReadValue();
-
         //modify variables when using the scroll wheel
         if(changeSize.y > 0)
         {
@@ -29,22 +23,18 @@ public class SquareSpawner : MonoBehaviour
             posOne--;
             negOne++;
         }
-
         //Four Vectors to add to mousePos
         Vector2 firstQuad = new Vector2(posOne, posOne);
         Vector2 secondQuad = new Vector2(posOne, negOne);
         Vector2 thirdQuad = new Vector2(negOne, negOne);
         Vector2 fourthQuad = new Vector2(negOne, posOne);
-
         //Turn mouse position into a Vector
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-
         //draw a sqaure always at the mouse position, square obscures when over objects closer to the camera
         Debug.DrawLine((mousePos + firstQuad), (mousePos + secondQuad), Color.white, 0, true);
         Debug.DrawLine((mousePos + secondQuad), (mousePos + thirdQuad), Color.white, 0, true);
         Debug.DrawLine((mousePos + thirdQuad), (mousePos + fourthQuad), Color.white, 0, true);
         Debug.DrawLine((mousePos + fourthQuad), (mousePos + firstQuad), Color.white, 0, true);
-
         //make a white square spawn at the mouse position if the left button of the mouse was pressed
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
