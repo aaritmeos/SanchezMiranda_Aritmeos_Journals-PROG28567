d0e1e3ef74853046296607ebc0943241ab364694
aaritmeos <sancheez@sheridancollege.ca>
2026-09-21 13:06:01 -0400

    Added work from Week 2 Class

---
+++ Assets/Journal 2/Controllers/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Transform enemyTransform;
+    public GameObject bombPrefab;
+    public List<Transform> asteroidTransforms;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.bKey.wasPressedThisFrame)
+        {
+            //SpawnBombAtOffset(Vector3.down);
+            StartCoroutine(SpawnBombAtOffset(Vector3.down));
+        }
+    }
+
+    IEnumerator SpawnBombAtOffset(Vector3 inOffset)
+    {
+        yield return new WaitForSeconds(3);
+        Instantiate(bombPrefab, transform.position + inOffset, Quaternion.identity);
+    }
+}
