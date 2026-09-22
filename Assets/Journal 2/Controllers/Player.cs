e230fe522fe9c8dae23c385e6a60498899cfc2f0
aaritmeos <sancheez@sheridancollege.ca>
2026-09-21 20:34:39 -0400

    Added function that allows the Player to spawn a bomb at a distance when B is pressed. Added a second function that allows the player to spawn a trail of bombs with consistent spacing by pressing T.

--- Assets/Journal 2/Controllers/Player.cs
+++ Assets/Journal 2/Controllers/Player.cs
@@ -8,20 +8,37 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public List<Transform> asteroidTransforms;
+    public Vector2 bombOffset; //Public Vector to modify how far is the bomb from the Player
+    public float bombTrailSpacing; //public float to determine space between bombs
+    public int numberOfTrailBombs; //public int to determine number of bombs
 
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current.bKey.wasPressedThisFrame)
+        if (Keyboard.current.bKey.wasPressedThisFrame) //run SpawnBombAtOffset Coroutine when B is pressed
         {
-            //SpawnBombAtOffset(Vector3.down);
-            StartCoroutine(SpawnBombAtOffset(Vector3.down));
+            StartCoroutine(SpawnBombAtOffset(bombOffset)); //bombOffset Vector as argument
+        }
+        if (Keyboard.current.tKey.wasPressedThisFrame) //run SpawnBombTrail when T is pressed
+        {
+            SpawnBombTrail(bombTrailSpacing, numberOfTrailBombs); //bombTrailSpacing and numberOfTrailBombs as arguments
         }
     }
 
-    IEnumerator SpawnBombAtOffset(Vector3 inOffset)
+    IEnumerator SpawnBombAtOffset(Vector3 inOffset) //Coroutine that spawns a bomb at a distance from the player
     {
-        yield return new WaitForSeconds(3);
-        Instantiate(bombPrefab, transform.position + inOffset, Quaternion.identity);
+        yield return new WaitForSeconds(3); //wait 3 seconds, then Instantiate
+        Instantiate(bombPrefab, transform.position + inOffset, Quaternion.identity); //use inOffset to distance bomb from the player
+    }
+
+    public void SpawnBombTrail (float inBombSpacing, int inNumberOfBombs) //method to spawn bomb trail at a distance from the player
+    {
+        float spacing = inBombSpacing; //float that determines the distance between bombs
+        for (int i = 0; i < inNumberOfBombs; i++) //loop uses inNumberOfBombs as limit
+        {
+            //Use spacing to distance bombs from player and themselves
+            Instantiate(bombPrefab, transform.position + new Vector3 (0, spacing, 0), Quaternion.identity); 
+            spacing = spacing + inBombSpacing; //add inBombSpacing to spacing each time the loop runs to keep consistent separation
+        }
     }
 }
